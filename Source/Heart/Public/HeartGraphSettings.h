bd6f9e71615abd397661013efb7cd5e9e8e73306
Guy Lundvall <47373221+Drakynfly@users.noreply.github.com>
2022-11-24 19:41:09 -0800

    Add settings class

---
+++ Source/Heart/Public/HeartGraphSettings.h
@@ -0,0 +1,25 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DeveloperSettings.h"
+#include "HeartGraphSettings.generated.h"
+
+/**
+ *
+ */
+UCLASS(config = Plugins, defaultconfig, DisplayName = "Heart Graph")
+class HEART_API UHeartGraphSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	//~ Begin UDeveloperSettings
+	virtual FName GetCategoryName() const override { return FName(TEXT("Plugins")); }
+	//~ End UDeveloperSettings
+
+	// Enables use of the Node Registry Subsystem at runtime. Imposes a small performance hit during startup, and a
+	// small memory footprint.
+	UPROPERTY(Config, EditAnywhere, Category = "Runtime")
+	bool CreateRuntimeNodeRegistry = false;
+};
