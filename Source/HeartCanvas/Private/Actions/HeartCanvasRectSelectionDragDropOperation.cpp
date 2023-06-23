c52178be596fff17235533d14d3a6bbef10794b6
Guy Lundvall <47373221+Drakynfly@users.noreply.github.com>
2023-06-23 16:52:50 -0700

    selection rect ddo

---
+++ Source/HeartCanvas/Private/Actions/HeartCanvasRectSelectionDragDropOperation.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#include "Actions/HeartCanvasRectSelectionDragDropOperation.h"
+
+#include "Components/CanvasPanelSlot.h"
+#include "UMG/HeartGraphCanvas.h"
+
+bool UHeartCanvasRectSelectionDragDropOperation::SetupDragDropOperation()
+{
+	Canvas = Cast<UHeartGraphCanvas>(SummonedBy);
+
+	if (!Canvas.IsValid())
+	{
+		return false;
+	}
+
+	if (FSlateApplication::IsInitialized())
+	{
+		SelectionStart = Canvas->GetTickSpaceGeometry()
+								.AbsoluteToLocal(FSlateApplication::Get().GetCursorPos());
+	}
+
+	return true;
+}
+
+void UHeartCanvasRectSelectionDragDropOperation::Dragged_Implementation(const FPointerEvent& PointerEvent)
+{
+	Super::Dragged_Implementation(PointerEvent);
+
+	const FVector2D CurrentLocation = Canvas->GetTickSpaceGeometry()
+					.AbsoluteToLocal(FSlateApplication::Get().GetCursorPos());
+
+
+	// Update visual
+
+	if (auto&& Slot = Cast<UCanvasPanelSlot>(DefaultDragVisual->Slot))
+	{
+
+	}
+
+
+	// Update node selection
+
+	if (auto&& Graph = Cast<UHeartGraphCanvas>(SummonedBy))
+	{
+
+	}
+}
---
+++ Source/HeartCanvas/Public/Actions/HeartCanvasRectSelectionDragDropOperation.h
@@ -0,0 +1,26 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#pragma once
+
+#include "HeartCanvasDragDropOperation.h"
+#include "HeartCanvasRectSelectionDragDropOperation.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class HEARTCANVAS_API UHeartCanvasRectSelectionDragDropOperation : public UHeartCanvasDragDropOperation
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool SetupDragDropOperation() override;
+
+	virtual void Dragged_Implementation(const FPointerEvent& PointerEvent) override;
+
+protected:
+	TWeakObjectPtr<UHeartGraphCanvas> Canvas;
+
+	// This is the point on the canvas initially dragged from.
+	FVector2D SelectionStart = FVector2D::ZeroVector;
+};
