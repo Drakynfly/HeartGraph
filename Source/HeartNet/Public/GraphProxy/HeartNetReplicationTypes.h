a6be274730532dd6f6f5a20ff19443af7ca3a620
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-03-11 20:18:12 -0700

    WIP net graph action execution

--- Source/HeartNet/Public/GraphProxy/HeartNetReplicationTypes.h
+++ Source/HeartNet/Public/GraphProxy/HeartNetReplicationTypes.h
@@ -4,6 +4,7 @@
 
 #include "HeartReplicateNodeData.h"
 #include "Model/HeartGraphPinReference.h"
+#include "UI/HeartInputActivation.h"
 #include "HeartNetReplicationTypes.generated.h"
 
 USTRUCT()
@@ -36,4 +37,68 @@ struct FHeartGraphConnectionEvent_Net
 
 	UPROPERTY()
 	TArray<FHeartNodeFlake> AffectedNodes;
+};
+
+USTRUCT()
+struct FHeartRemoteGraphActionArguments
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	FHeartGraphPinReference NodeAndPinGuid;
+
+	// Custom target for executing actions on pins.
+	UPROPERTY()
+	TObjectPtr<UObject> PinTarget;
+
+	UPROPERTY()
+	FHeartManualEvent Activation;
+
+	// This object has to exist on the server for it to be replicated. Heart does not handle this automatically.
+	UPROPERTY()
+	TObjectPtr<UObject> ContextObject;
+
+	bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+	{
+		uint8 HasValidNodeGuid = NodeAndPinGuid.NodeGuid.IsValid();
+		uint8 HasValidPinGuid = NodeAndPinGuid.PinGuid.IsValid();
+
+		if (Ar.IsSaving())
+		{
+			HasValidNodeGuid = NodeAndPinGuid.NodeGuid.IsValid();
+			HasValidPinGuid = NodeAndPinGuid.PinGuid.IsValid();
+		}
+
+		// Serialize single bits for which guids are valid
+		Ar.SerializeBits(&HasValidNodeGuid, 1);
+		Ar.SerializeBits(&HasValidPinGuid, 1);
+
+		if (HasValidNodeGuid)
+		{
+			Ar << NodeAndPinGuid.NodeGuid;
+		}
+
+		if (HasValidPinGuid)
+		{
+			Ar << NodeAndPinGuid.PinGuid;
+		}
+
+		Ar << PinTarget;
+
+		FHeartManualEvent::StaticStruct()->GetCppStructOps()->NetSerialize(Ar, Map, bOutSuccess, &Activation);
+
+		Ar << ContextObject;
+
+		bOutSuccess &= !Ar.IsError();
+		return bOutSuccess;
+	}
+};
+
+template<>
+struct TStructOpsTypeTraits<FHeartRemoteGraphActionArguments> : public TStructOpsTypeTraitsBase2<FHeartRemoteGraphActionArguments>
+{
+	enum
+	{
+		WithDeltaSerializer = true,
+	};
 };
