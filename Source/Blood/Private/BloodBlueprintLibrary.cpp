e63345d2f9294baf776271ec555e28e9051e5155
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-03-20 13:17:48 -0700

    The single line of code to make 5.4 compile

--- Source/Blood/Private/BloodBlueprintLibrary.cpp
+++ Source/Blood/Private/BloodBlueprintLibrary.cpp
@@ -1,6 +1,7 @@
 ﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
 
 #include "BloodBlueprintLibrary.h"
+#include "Blueprint/BlueprintExceptionInfo.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(BloodBlueprintLibrary)
 
