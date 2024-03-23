d91d38fd7b033c78ace8cc7c2abd05e684ce9c6c
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-03-22 22:38:55 -0700

    Implement undo for AutoLayout

--- Source/HeartCanvas/Private/Actions/HeartCanvasAction_AutoLayout.cpp
+++ Source/HeartCanvas/Private/Actions/HeartCanvasAction_AutoLayout.cpp
@@ -1,6 +1,8 @@
 ﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
 
 #include "Actions/HeartCanvasAction_AutoLayout.h"
+#include "Model/HeartGraph.h"
+#include "ModelView/HeartActionHistory.h"
 #include "UMG/HeartGraphCanvas.h"
 #include "ModelView/HeartLayoutHelper.h"
 
@@ -9,28 +11,56 @@
 FEventReply UHeartCanvasAction_AutoLayout::ExecuteOnGraph(UHeartGraphCanvas* CanvasGraph, const FHeartInputActivation& Activation,
 	UObject* ContextObject)
 {
-	const UHeartLayoutHelper* LayoutHelper = nullptr;
+	const UHeartLayoutHelper* LayoutHelper = Cast<UHeartLayoutHelper>(ContextObject);
 
-	if (const UHeartLayoutHelper* AsObject = Cast<UHeartLayoutHelper>(ContextObject))
+	if (!IsValid(LayoutHelper))
 	{
-		LayoutHelper = AsObject;
-	}
-
-	if (UClass* AsClass = Cast<UClass>(ContextObject))
-	{
-		const TSubclassOf<UHeartLayoutHelper> LayoutClass = AsClass;
-		if (IsValid(LayoutClass) && !LayoutClass->HasAnyClassFlags(CLASS_Abstract))
+		if (const TSubclassOf<UHeartLayoutHelper> LayoutClass = Cast<UClass>(ContextObject);
+			IsValid(LayoutClass) && !LayoutClass->HasAnyClassFlags(CLASS_Abstract))
 		{
 			LayoutHelper = GetDefault<UHeartLayoutHelper>(LayoutClass);
 		}
 	}
 
-	if (IsValid(LayoutHelper))
+	if (!IsValid(LayoutHelper))
 	{
-		LayoutHelper->Layout(CanvasGraph);
-		return true;
+		UE_LOG(LogHeartGraphCanvas, Error, TEXT("UHeartCanvasAction_AutoLayout expects a context action that is a instance or class pointer of a UHeartLayoutHelper type."))
+		return false;
 	}
 
-	UE_LOG(LogHeartGraphCanvas, Error, TEXT("UHeartCanvasAction_AutoLayout expects a context action that is a instance or class pointer of a UHeartLayoutHelper type."))
-	return false;
+	if (Heart::Action::History::IsUndoable())
+	{
+		CanvasGraph->GetGraph()->ForEachNode(
+			[&](const UHeartGraphNode* Node)
+			{
+				OriginalLocations.Add(Node->GetGuid(), Node->GetLocation());
+				return true;
+			});
+	}
+
+	LayoutHelper->Layout(CanvasGraph);
+	return true;
+}
+
+bool UHeartCanvasAction_AutoLayout::Undo(UObject* Target)
+{
+	UHeartGraphCanvas* GraphCanvas = Cast<UHeartGraphCanvas>(Target);
+	if (!IsValid(GraphCanvas))
+	{
+		return false;
+	}
+
+	UHeartGraph* Graph = GraphCanvas->GetHeartGraph();
+
+	TSet<UHeartGraphNode*> Touched;
+
+	for (auto&& OriginalLocation : OriginalLocations)
+	{
+		Touched.Add(Graph->GetNode(OriginalLocation.Key));
+		GraphCanvas->SetNodeLocation(OriginalLocation.Key, OriginalLocation.Value, false);
+	}
+
+	Graph->NotifyNodeLocationsChanged(Touched, false);
+
+	return true;
 }
