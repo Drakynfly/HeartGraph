58264ab333948e87c047c681abaae15c74f28799
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-03-22 22:39:24 -0700

    Implement undo for DisconnectPins

--- Source/Heart/Private/ModelView/Actions/HeartAction_DisconnectPins.cpp
+++ Source/Heart/Private/ModelView/Actions/HeartAction_DisconnectPins.cpp
@@ -6,6 +6,7 @@
 #include "Model/HeartGraphPinInterface.h"
 
 #include "Model/HeartGraphNode.h"
+#include "ModelView/HeartActionHistory.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(HeartAction_DisconnectPins)
 
@@ -18,14 +19,47 @@ bool UHeartAction_DisconnectPins::CanExecute(const UObject* Object) const
 void UHeartAction_DisconnectPins::ExecuteOnPin(const TScriptInterface<IHeartGraphPinInterface>& Pin,
 											   const FHeartInputActivation& Activation, UObject* ContextObject)
 {
-	if (const UHeartGraphNode* Node = Pin->GetHeartGraphNode())
+	if (UHeartGraphNode* Node = Pin->GetHeartGraphNode())
 	{
-		Node->GetGraph()->EditConnections().DisconnectAll({Node->GetGuid(), Pin->GetPinGuid()});
+		const FHeartGraphPinReference PinRef{Node->GetGuid(), Pin->GetPinGuid()};
+
+		if (Heart::Action::History::IsUndoable())
+		{
+			TargetNode = Node;
+			Node->GetGraph()->EditConnections().CreateMementos(PinRef, Mementos).DisconnectAll(PinRef);
+		}
+		else
+		{
+			// Quick path when not undoable; don't bother caching mementos
+			Node->GetGraph()->EditConnections().DisconnectAll(PinRef);
+		}
 	}
 }
 
 void UHeartAction_DisconnectPins::ExecuteOnNode(UHeartGraphNode* Node, const FHeartInputActivation& Activation,
 												UObject* ContextObject)
 {
-	Node->GetGraph()->EditConnections().DisconnectAll(Node->GetGuid());
+	const FHeartNodeGuid Guid = Node->GetGuid();
+
+	if (Heart::Action::History::IsUndoable())
+	{
+		TargetNode = Node;
+		Node->GetGraph()->EditConnections().CreateAllMementos(Guid, Mementos).DisconnectAll(Guid);
+	}
+	else
+	{
+		// Quick path when not undoable; don't bother caching mementos
+		Node->GetGraph()->EditConnections().DisconnectAll(Guid);
+	}
+}
+
+bool UHeartAction_DisconnectPins::Undo(UObject* Target)
+{
+	if (TargetNode.IsValid())
+	{
+		TargetNode->GetGraph()->EditConnections().RestoreMementos(Mementos);
+		return true;
+	}
+
+	return false;
 }
--- Source/Heart/Public/ModelView/Actions/HeartAction_DisconnectPins.h
+++ Source/Heart/Public/ModelView/Actions/HeartAction_DisconnectPins.h
@@ -3,8 +3,10 @@
 #pragma once
 
 #include "HeartGraphAction.h"
+#include "Model/HeartPinConnectionEdit.h"
 #include "HeartAction_DisconnectPins.generated.h"
 
+struct FHeartGraphPinReference;
 class IHeartGraphPinInterface;
 
 /**
@@ -19,4 +21,11 @@ protected:
 	virtual bool CanExecute(const UObject* Object) const override;
 	virtual void ExecuteOnPin(const TScriptInterface<IHeartGraphPinInterface>&, const FHeartInputActivation& Activation, UObject* ContextObject) override;
 	virtual void ExecuteOnNode(UHeartGraphNode* Node, const FHeartInputActivation& Activation, UObject* ContextObject) override;
+	virtual bool CanUndo(UObject* Target) const override { return true; }
+	virtual bool Undo(UObject* Target) override;
+
+private:
+	// Undo data
+	TWeakObjectPtr<UHeartGraphNode> TargetNode;
+	TMap<FHeartNodeGuid, Heart::Connections::FEdit::FMemento> Mementos;
 };
