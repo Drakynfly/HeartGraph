c743399d46006fb0736dcd5cbb500ab4676c9abc
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2024-04-11 13:30:12 -0700

    Add base class for actions that operate on a slate View

---
+++ Source/HeartCanvas/Private/Actions/HeartGraphSlateAction.cpp
@@ -0,0 +1,141 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#include "Actions/HeartGraphSlateAction.h"
+#include "Input/HeartSlateReplyWrapper.h"
+#include "Input/SlatePointerWrappers.h"
+#include "ModelView/HeartActionHistory.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(HeartGraphSlateAction)
+
+FText UHeartGraphSlateAction::GetDescription(const UObject* Target) const
+{
+	if (auto&& SlateTarget = Cast<UHeartSlatePtr>(Target))
+	{
+		return GetDescription(SlateTarget);
+	}
+	return Super::GetDescription(Target);
+}
+
+bool UHeartGraphSlateAction::CanExecute(const UObject* Target) const
+{
+	if (auto&& SlateTarget = Cast<UHeartSlatePtr>(Target))
+	{
+		return CanExecuteOnSlatePtr(SlateTarget);
+	}
+	return false;
+}
+
+FHeartEvent UHeartGraphSlateAction::Execute(const Heart::Action::FArguments& Arguments) const
+{
+	auto&& Widget = Cast<UHeartSlatePtr>(Arguments.Target);
+
+	if (!IsValid(Widget))
+	{
+		return FHeartEvent::Invalid;
+	}
+
+	return Heart::Action::History::Log(this, Arguments,
+		[&](FBloodContainer& UndoData)
+		{
+			return ExecuteOnSlatePtr(Widget, Arguments.Activation, Arguments.Payload, UndoData);
+		});
+}
+
+FText UHeartGraphSlateAction::GetDescription(const UHeartSlatePtr* SlatePtr) const
+{
+	return FText::GetEmpty();
+}
+
+bool UHeartGraphSlateAction::CanExecuteOnSlatePtr(const UHeartSlatePtr* SlatePtr) const
+{
+	return true;
+}
+
+FHeartEvent UHeartGraphSlateAction::ExecuteOnSlatePtr(UHeartSlatePtr* SlatePtr, const FHeartInputActivation& Activation,
+													 UObject* ContextObject, FBloodContainer& UndoData) const
+{
+	if (!IsValid(SlatePtr))
+	{
+		return FHeartEvent::Invalid;
+	}
+
+	if (auto&& Graph = Cast<UHeartSlateGraph>(SlatePtr))
+    {
+    	return UHeartSlateReplyWrapper::ReplyEventToHeartEvent(FHeartEvent::Handled,
+    		ExecuteOnGraph(Graph, Activation, ContextObject, UndoData));
+    }
+
+    if (auto&& Node = Cast<UHeartSlateNode>(SlatePtr))
+    {
+    	return UHeartSlateReplyWrapper::ReplyEventToHeartEvent(FHeartEvent::Handled,
+    		ExecuteOnNode(Node, Activation, ContextObject, UndoData));
+    }
+
+    if (auto&& Pin = Cast<UHeartSlatePin>(SlatePtr))
+    {
+    	return UHeartSlateReplyWrapper::ReplyEventToHeartEvent(FHeartEvent::Handled,
+    		ExecuteOnPin(Pin, Activation, ContextObject, UndoData));
+    }
+
+	return FHeartEvent::Ignored;
+}
+
+FText UHeartGraphSlateAction_BlueprintBase::GetDescription(const UHeartSlatePtr* SlatePtr) const
+{
+	return BP_GetDescription(SlatePtr);
+}
+
+bool UHeartGraphSlateAction_BlueprintBase::CanExecuteOnSlatePtr(const UHeartSlatePtr* SlatePtr) const
+{
+	if (ensure(IsValid(SlatePtr)))
+	{
+		if (GetClass()->IsFunctionImplementedInScript(GET_FUNCTION_NAME_CHECKED(ThisClass, BP_CanExecuteOnWidget)))
+		{
+			return BP_CanExecuteOnWidget(SlatePtr);
+		}
+	}
+	return true;
+}
+
+FEventReply UHeartGraphSlateAction_BlueprintBase::ExecuteOnGraph(UHeartSlateGraph* Graph, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const
+{
+	if (ensure(IsValid(Graph)))
+	{
+		return BP_ExecuteOnGraph(Graph, Activation, ContextObject, UndoData);
+	}
+	return false;
+}
+
+FEventReply UHeartGraphSlateAction_BlueprintBase::ExecuteOnNode(UHeartSlateNode* Node, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const
+{
+	if (ensure(IsValid(Node)))
+	{
+		return BP_ExecuteOnNode(Node, Activation, ContextObject, UndoData);
+	}
+	return false;
+}
+
+FEventReply UHeartGraphSlateAction_BlueprintBase::ExecuteOnPin(UHeartSlatePin* Pin, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const
+{
+	if (ensure(IsValid(Pin)))
+	{
+		return BP_ExecuteOnPin(Pin, Activation, ContextObject, UndoData);
+	}
+	return false;
+}
+
+bool UHeartGraphSlateAction_BlueprintBase::CanUndo(const UObject* Target) const
+{
+	// A Blueprint action can undo if it has an implementation of BP_Undo
+	return GetClass()->IsFunctionImplementedInScript(GET_FUNCTION_NAME_CHECKED(ThisClass, BP_Undo));
+}
+
+bool UHeartGraphSlateAction_BlueprintBase::Undo(UObject* Target, const FBloodContainer& UndoData) const
+{
+	if (auto&& WidgetTarget = Cast<UHeartSlatePtr>(Target))
+	{
+		return BP_Undo(WidgetTarget, UndoData);
+	}
+
+	return false;
+}
---
+++ Source/HeartCanvas/Public/Actions/HeartGraphSlateAction.h
@@ -0,0 +1,81 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#pragma once
+
+#include "BloodContainer.h"
+#include "Components/SlateWrapperTypes.h"
+#include "Input/HeartActionBase.h"
+#include "HeartGraphSlateAction.generated.h"
+
+class UHeartSlatePtr;
+class UHeartSlateGraph;
+class UHeartSlateNode;
+class UHeartSlatePin;
+
+/**
+ *
+ */
+UCLASS(Abstract, BlueprintType)
+class HEARTCANVAS_API UHeartGraphSlateAction : public UHeartActionBase
+{
+	GENERATED_BODY()
+
+protected:
+	//~ UHeartActionBase
+	virtual FText GetDescription(const UObject* Target) const override final;
+	virtual bool CanExecute(const UObject* Target) const override final;
+	virtual FHeartEvent Execute(const Heart::Action::FArguments& Arguments) const override final;
+	//~ UHeartActionBase
+
+	virtual FText GetDescription(const UHeartSlatePtr* SlatePtr) const;
+	virtual bool CanExecuteOnSlatePtr(const UHeartSlatePtr* SlatePtr) const;
+	virtual FHeartEvent ExecuteOnSlatePtr(UHeartSlatePtr* SlatePtr, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const;
+
+	virtual FEventReply ExecuteOnGraph(UHeartSlateGraph* SlateGraph, const FHeartInputActivation& Activation,
+		UObject* ContextObject, FBloodContainer& UndoData) const { return false; }
+
+	virtual FEventReply ExecuteOnNode(UHeartSlateNode* SlateNode, const FHeartInputActivation& Activation,
+		UObject* ContextObject, FBloodContainer& UndoData) const { return false; }
+
+	virtual FEventReply ExecuteOnPin(UHeartSlatePin* SlatePin, const FHeartInputActivation& Activation,
+		UObject* ContextObject, FBloodContainer& UndoData) const { return false; }
+};
+
+
+UCLASS(Abstract, Blueprintable, MinimalAPI)
+class UHeartGraphSlateAction_BlueprintBase final : public UHeartGraphSlateAction
+{
+	GENERATED_BODY()
+
+protected:
+	//~ UHeartActionBase
+	virtual bool CanUndo(const UObject* Target) const override;
+	virtual bool Undo(UObject* Target, const FBloodContainer& UndoData) const override;
+	//~ UHeartActionBase
+
+	//~ UHeartGraphSlateAction
+	virtual FText GetDescription(const UHeartSlatePtr* SlatePtr) const override;
+	virtual bool CanExecuteOnSlatePtr(const UHeartSlatePtr* SlatePtr) const override;
+	virtual FEventReply ExecuteOnGraph(UHeartSlateGraph* Graph, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const override;
+	virtual FEventReply ExecuteOnNode(UHeartSlateNode* Node, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const override;
+	virtual FEventReply ExecuteOnPin(UHeartSlatePin* Pin, const FHeartInputActivation& Activation, UObject* ContextObject, FBloodContainer& UndoData) const override;
+	//~ UHeartGraphSlateAction
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Get Description"))
+	FText BP_GetDescription(const UHeartSlatePtr* SlatePtr) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Can Execute on Widget"))
+	bool BP_CanExecuteOnWidget(const UHeartSlatePtr* SlatePtr) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Execute on Graph"))
+	FEventReply BP_ExecuteOnGraph(UHeartSlateGraph* SlateGraph, const FHeartInputActivation& Activation, UObject* ContextObject, UPARAM(ref) FBloodContainer& UndoData) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Execute on Node"))
+	FEventReply BP_ExecuteOnNode(UHeartSlateNode* SlateNode, const FHeartInputActivation& Activation, UObject* ContextObject, UPARAM(ref) FBloodContainer& UndoData) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Execute on Pin"))
+	FEventReply BP_ExecuteOnPin(UHeartSlatePin* SlatePin, const FHeartInputActivation& Activation, UObject* ContextObject, UPARAM(ref) FBloodContainer& UndoData) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Undo"))
+	bool BP_Undo(UHeartSlatePtr* Target, const FBloodContainer& UndoData) const;
+};
